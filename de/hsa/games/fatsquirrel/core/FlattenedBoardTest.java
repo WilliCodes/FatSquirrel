a8708d60793e3f7fc44a1ab3126198b4991be37d
Genwar <38666279+Genwar@users.noreply.github.com>
2018-06-05 00:02:33 +0200

    updated FlattenBoardTest

--- de/hsa/games/fatsquirrel/core/FlattenedBoardTest.java
+++ de/hsa/games/fatsquirrel/core/FlattenedBoardTest.java
@@ -1,21 +1,27 @@
 package de.hsa.games.fatsquirrel.core;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 public class FlattenedBoardTest {
 
 
-    
+    @Mock
 	private FlattenedBoard flatBoardTest;
 	private BoardConfig config;
 	
 	@Before
 	public void setUp() {
-		BoardConfig config = Mockito.mock(BoardConfig.class);
+	    MockitoAnnotations.initMocks(this);
+		config = Mockito.mock(BoardConfig.class);
 		flatBoardTest = Mockito.mock(FlattenedBoard.class);
 	}
 
@@ -26,7 +32,14 @@ public class FlattenedBoardTest {
 	}
 	
 	@Test
-	public void tryMoveTest() {
+	public void tryMoveMasterTest() {
 	    
-	}
+	    MasterSquirrel masterMock = mock(MasterSquirrel.class);
+	    when(masterMock.getPosition()).thenReturn(new XY(2, 2));
+	    flatBoardTest.tryMove(masterMock, XY.DOWN);
+	    verify(masterMock).setPosition(new XY(1,2));
+	    }
+	
+	
+	
 }
